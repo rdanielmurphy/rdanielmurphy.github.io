526210b42272941bc635d9e9774099e5e86743f8
Daniel Murphy <rdanielmurphy@gmail.com>
2015-10-19 14:32:30 -0400

    More work on SentI

--- js/senti.js
+++ js/senti.js
@@ -11,3 +11,64 @@ L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
 L.marker([37.09024, -95.712891]).addTo(map)
 	.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
 	.openPopup();
+
+
+/*
+makeCorsRequest();
+
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+// Make the actual CORS request.
+function makeCorsRequest() {
+  var url = 'http://sentiment-meltingpotapp.rhcloud.com/asciimo';
+
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    alert(text);
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
+}
+*/
+
+
+$.ajax({
+  type: 'GET',
+  dataType: 'jsonp',
+  jsonpCallback: 'callback',
+  url: 'http://sentiment-meltingpotapp.rhcloud.com/asciimo',
+  data: { },
+  beforeSend:function(){
+  },
+  success:function(data){
+    alert(data);
+  },
+  error:function(){
+    alert("ERROR");
+  }
+});
