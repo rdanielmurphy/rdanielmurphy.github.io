c46043914f62ec832fab769556e1a92867ac1956
Daniel Murphy <rdanielmurphy@gmail.com>
2015-10-20 01:28:33 -0400

    Finished up SenTy

--- js/senti.js
+++ js/senti.js
@@ -9,7 +9,9 @@ L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
 }).addTo(map);
 
 $( "#runQueryBtn" ).click(function() {
-	var search = $( "#query" ).val();
+	var search = $( "#query" ).val().replace(/\s/g, '').replace(/[^a-z0-9]/gi,'');;
+	if (search.length === 0)
+		return;
 	var apicall = "http://sentiment-meltingpotapp.rhcloud.com/gettweets/";
 	var eastqueryurl = apicall + "east/" + search;
 	var centerqueryurl = apicall + "center/" + search;
@@ -25,10 +27,14 @@ $( "#runQueryBtn" ).click(function() {
 	  	jsonpCallback: 'callback',
 	  	url: eastqueryurl,
 	  	data: { },
-	  	beforeSend:function(){
-	  	},
 	  	success:function(data){
-	    	renderSentiment(data, eastMarkersList);
+	  		console.log(data.length);
+	  	},
+	  	error:function(jqxhr){
+	    	console.log("No Results in east");
+	  	},
+	  	complete: function(data) {
+	  		renderSentiment(data.responseJSON, eastMarkersList);
 
 	    	//center
 	    	$.ajax({
@@ -37,10 +43,14 @@ $( "#runQueryBtn" ).click(function() {
 			  	jsonpCallback: 'callback',
 			  	url: centerqueryurl,
 			  	data: { },
-			  	beforeSend:function(){
+			  	success:function(data) {
+			  		console.log(data.length);
 			  	},
-			  	success:function(data){
-			    	renderSentiment(data, centerMarkersList);
+			  	error:function(jqxhr){
+			    	console.log("No Results in center");
+			  	},
+			  	complete:function(data){
+			    	renderSentiment(data.responseJSON, centerMarkersList);
 
 			    	//west
 			    	$.ajax({
@@ -49,27 +59,19 @@ $( "#runQueryBtn" ).click(function() {
 					  	jsonpCallback: 'callback',
 					  	url: westqueryurl,
 					  	data: { },
-					  	beforeSend:function(){
-					  	},
 					  	success:function(data){
-					  		spinner.stop();
-					    	renderSentiment(data, westMarkersList);
+					  		console.log(data.length);
 					  	},
 					  	error:function(jqxhr){
+					    	console.log("No Results in west");
+					  	},
+					  	complete:function(data) {
 					  		spinner.stop();
-					    	alert("There was an error.  Try a different search.");
+					  		renderSentiment(data.responseJSON, westMarkersList);
 					  	}
 					});
-			  	},
-			  	error:function(jqxhr){
-			  		spinner.stop();
-			    	alert("There was an error.  Try a different search.");
 			  	}
 			});
-	  	},
-	  	error:function(jqxhr){
-	  		spinner.stop();
-	    	alert("There was an error.  Try a different search.");
 	  	}
 	});
 });
@@ -83,21 +85,47 @@ function renderSentiment(tweets, markersList) {
          	map.removeLayer(markersList[i]);
         }
     }
-    for (var i = 0; i< tweets.length; i++) {
-        var tweet = tweets[i];
-        if (tweet.score !== undefined) {
-            var markercolor = "#DC143C";
-            if (tweet.score == 0)
-             	markercolor = "#FFFACD";
-            else if (tweet.score > 0)
-             	markercolor = "#32CD32";
+    if (tweets) {
+    	for (var i = 0; i< tweets.length; i++) {
+	        var tweet = tweets[i];
+	        if (tweet.score !== undefined) {
+	            var markercolor = "#DC143C";
+	            if (tweet.score == 0)
+	             	markercolor = "#FFFACD";
+	            else if (tweet.score > 0)
+	             	markercolor = "#32CD32";
 
-            if (tweet.location) {
-             	var icon = L.MakiMarkers.icon({color: markercolor});
-             	var pin = L.marker([tweet.location.coordinates[0], tweet.location.coordinates[1]], {icon: icon});
-             	pin.bindPopup(tweet.text).addTo(map);
-             	markersList.push(pin);
-            }
-        }
-    }
+	            if (tweet.location) {
+	             	var icon = L.MakiMarkers.icon({color: markercolor});
+	             	var pin = L.marker([tweet.location.coordinates[0], tweet.location.coordinates[1]], {icon: icon});
+	             	pin.bindPopup(tweet.text).addTo(map);
+	             	markersList.push(pin);
+	            }
+	        }
+    	}
+	}
 }
+
+function getColor(d) {
+    return d < 0 ? '#DC143C' :
+           d > 0 ? '#32CD32' :
+           		   '#FFFACD' ;
+}
+
+//legend
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [-1,0,1],
+        labels = ["Negative Sentiment", "Neutral Sentiment", "Positive Sentiment"];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i]) + '"></i> ' +
+            labels[i] + '<br>';
+    }
+
+    return div;
+};
+legend.addTo(map);
--- senti.md
+++ senti.md
@@ -6,7 +6,21 @@ backgroundpic: sentymap.jpg
 includeLeaflet: true
 ---
 
-<label for="usr">Public Sentiment Query:</label>
+SenTy uses sentiment analysis and social media platforms like Twitter to find positive, neutral, and negative sentiments across the US based on various topics.  Type in a company name, a sports team name, a person's name, even a food and find what the current sentiment of the subject is.  The possibilities really are endless.  
+
+On the backend, SenTy grabs geolocated social media posts inside the continental US that fit the search criteria, and runs sentiment analysis on them.  Then once that's finished, returns it for the frontend to render it on a map.  
+
+
+### Tech
+The backend server is using NodeJs, HP Haven Big Data analytics platform to perform sentiment analysis, and Twitter API to get geolocated tweets inside the continental US.  The frontend is using Leaflet and OpenStreetMap to display the results on a map.
+
+### Future
+To improve the tool, more social media platforms could be leveraged.  Currently it only shows tweets from the last 7 days since that's all the Twitter API allows, but it would become a very powerful tool if it could get tweets from further in the past.  Then it could show trends over time, perhaps in a heat map visualization.
+
+<br>
+<br>
+<br>
+<label>Public Sentiment Query:</label>
 <div class="input-group">
    	<input type="text" class="form-control" id="query" value="Uber">
    	<span class="input-group-btn">
@@ -15,3 +29,7 @@ includeLeaflet: true
 </div>
 <br>
 <div id="map" style="height: 700px;"><div id="progress"></id></div>
+
+<br>
+<br>
+
